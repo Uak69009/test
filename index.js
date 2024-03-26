4cb61a609139eed6e7e751eb3782df9f24239c32
umair amjad khan <138983630+Uak69009@users.noreply.github.com>
2024-03-26 22:47:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter the first number", type: "number", name: "firstnumber" },
+    { message: "enter the second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
---
+++ tody.js
@@ -0,0 +1,13 @@
+let num = 10;
+let num2 = 20;
+if (num < num2) {
+    console.log("num is less then num 2");
+}
+else if (num == num2) {
+    console.log("num is equal to num2");
+}
+else {
+    console.log("num is not less then num2");
+}
+export {};
+//presentation example if else 
